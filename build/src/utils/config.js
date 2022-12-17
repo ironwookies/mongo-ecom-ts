951e48b81b2a3525e33e82da8d8da7019c1329ba
Andre Cabrera <cabrerandre@gmail.com>
2022-12-17 13:58:57 -0500

    remvoing build folder

--- build/src/utils/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = exports.appPort = exports.db_uri = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.db_uri = (_a = process.env.MONGODB_URI) !== null && _a !== void 0 ? _a : '';
-exports.appPort = process.env.PORT;
-exports.env = process.env.ENV;
